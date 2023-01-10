4edab61c44b6b4ffa54a28c3093c575c8fc3f13e
Allan Deutsch <allan@allandeutsch.com>
2023-01-10 14:38:29 -0800

    Fixed invalid link a11y issue

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -71,7 +71,7 @@
 			{#each navItems as navItem}
 				{#if 'children' in navItem}
 					<li>
-						<a href="#">
+						<span>
 							{navItem.title}
 							<svg
 								class="fill-current"
@@ -81,7 +81,7 @@
 								viewBox="0 0 24 24"
 								><path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z" /></svg
 							>
-						</a>
+						</span>
 						<ul class="p-2 bg-base-100">
 							{#each navItem.children as child, i}
 								<li><a tabindex={i} href={child.href}>{child.title}</a></li>
